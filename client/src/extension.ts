7caba9177d3448429ef7d1905d2a9132c1833c0b
Magus <magus@bgforge.net>
2018-11-22 21:31:05 +0700

    let max num of problems work after all

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -71,7 +71,7 @@ export function activate(context: ExtensionContext) {
 
 	// Create the language client and start the client.
 	client = new LanguageClient(
-		'SSLlanguageServer',
+		'ssl',
 		'SSL Language Server',
 		serverOptions,
 		clientOptions
--- server/src/server.ts
+++ server/src/server.ts
@@ -85,27 +85,27 @@ connection.onInitialized(() => {
 	});
 });
 
-// The example settings
-interface ExampleSettings {
+// The settings
+interface SSLsettings {
 	maxNumberOfProblems: number;
 }
 
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
 // Please note that this is not the case when using this server with the client provided in this example
 // but could happen with other clients.
-const defaultSettings: ExampleSettings = { maxNumberOfProblems: 10 };
-let globalSettings: ExampleSettings = defaultSettings;
+const defaultSettings: SSLsettings = { maxNumberOfProblems: 10 };
+let globalSettings: SSLsettings = defaultSettings;
 
 // Cache the settings of all open documents
-let documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
+let documentSettings: Map<string, Thenable<SSLsettings>> = new Map();
 
 connection.onDidChangeConfiguration(change => {
 	if (hasConfigurationCapability) {
 		// Reset all cached document settings
 		documentSettings.clear();
 	} else {
-		globalSettings = <ExampleSettings>(
-			(change.settings.SSLlanguageServer || defaultSettings)
+		globalSettings = <SSLsettings>(
+			(change.settings.ssl || defaultSettings)
 		);
 	}
 
@@ -113,7 +113,7 @@ connection.onDidChangeConfiguration(change => {
 	documents.all().forEach(validateTextDocument);
 });
 
-function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
+function getDocumentSettings(resource: string): Thenable<SSLsettings> {
 	if (!hasConfigurationCapability) {
 		return Promise.resolve(globalSettings);
 	}
@@ -121,7 +121,7 @@ function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
 	if (!result) {
 		result = connection.workspace.getConfiguration({
 			scopeUri: resource,
-			section: 'SSL configuration'
+			section: 'ssl'
 		});
 		documentSettings.set(resource, result);
 	}
