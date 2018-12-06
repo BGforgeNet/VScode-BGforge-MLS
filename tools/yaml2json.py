b3f78dc22c18e12fc6863fb8c9c451e0a4f1a9d6
Magus <magus@bgforge.net>
2018-12-06 10:14:57 +0700

    allow for shorthand syntax in yaml

--- tools/yaml2json.py
+++ tools/yaml2json.py
@@ -5,5 +5,13 @@ import sys, yaml, json
 
 with open(sys.argv[1]) as yf:
   data = yaml.load(yf)
+
+for rep_item in data['repository']: # allow to use shorthand syntax in yaml
+  rep_data = data['repository'][rep_item]
+  if ('name' in rep_data) and ('patterns' in rep_data):
+    for item in rep_data['patterns']:
+      if not 'name' in item:
+        item['name'] = rep_data['name']
+
 with open(sys.argv[2], 'w') as jf:
   json.dump(data, jf, indent=2)
