62c812cff5cb7b6f2a0f277b1f40397637987923
Magus <magus@bgforge.net>
2019-06-28 16:15:31 +0700

    add script for ascii conversion

---
+++ scripts/to-ascii.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -xeu -o pipefail
+
+tools_dir="scripts"
+completion_dir="server/out"
+
+for yaml_file in $(ls $completion_dir | grep -i "\.yml"); do
+  sed -i "s|’|'|g" "$completion_dir/$yaml_file"
+  sed -i "s|–|-|g" "$completion_dir/$yaml_file"
+done
+
+wait
