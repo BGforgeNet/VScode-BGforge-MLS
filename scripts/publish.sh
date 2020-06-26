31c7ddb5e954ba88bade36c99aa693d48d2df255
Magus <magus@bgforge.net>
2020-06-26 23:08:29 +0700

    updated repo url in script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,7 +7,7 @@ vsce publish
 vsce package
 
 version="$(jq -r '.version' package.json)"
-githubrelease release BGforgeNet/vscode-bgforge-mls create "v$version" --publish --name "v$version" "bgforge-mls-$version.vsix"
+githubrelease release BGforgeNet/VScode-BGforge-MLS create "v$version" --publish --name "v$version" "bgforge-mls-$version.vsix"
 rm -f *.vsix
 
 #for next release
