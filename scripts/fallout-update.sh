4c0403d4dcb0eb4eb03e5d616cd515c083ab2d6d
Magus <magus@bgforge.net>
2023-01-20 08:11:41 +0700

    drop loading rpu defines statically and into highlight

--- scripts/fallout-update.sh
+++ scripts/fallout-update.sh
@@ -7,27 +7,12 @@ set -xeu -o pipefail
 external="external/fallout"
 sfall_repo="https://github.com/BGforgeNet/sfall.git"
 sfall_dir="sfall"
-rpu_repo="https://github.com/BGforgeNet/Fallout2_Restoration_Project"
-rpu_dir="rpu"
 sfall_file="server/data/fallout-ssl-sfall.yml"
 highlight_file="syntaxes/fallout-ssl.tmLanguage.yml"
 
 if [ ! -d $external ]; then
     mkdir $external
 fi
-
-# rpu
-pushd .
-cd $external
-if [ ! -d $rpu_dir ]; then
-    git clone $rpu_repo $rpu_dir
-fi
-cd $rpu_dir
-git checkout master
-git pull
-last_v="v$(git tag | grep "^v" | sed 's|^v||' | sort -n | tail -1)"
-git checkout "$last_v"
-popd
 
 # sfall
 pushd .
@@ -43,5 +28,5 @@ git checkout "$last_v"
 popd
 
 ./scripts/fallout_update.py -s "$external" --sfall-file "$sfall_file" --highlight-file "$highlight_file"
-
-./scripts/syntaxes_to_json.sh
+pwd
+./scripts/syntaxes-to-json.sh
