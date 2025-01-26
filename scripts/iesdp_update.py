3ac62396788dd83b8a3b1ae5b514a70be58eee0e
Magus <magus@bgforge.net>
2025-01-26 16:22:58 +0700

    Make pylint happy

--- scripts/iesdp_update.py
+++ scripts/iesdp_update.py
@@ -311,8 +311,8 @@ for ff in formats:
         definition_items = {**definition_items, **new_definition_items}
 
         pod.append_offsets(offsets, prefix)
-    prefix_dir = get_format_version(ff)
-    dump_definition(prefix_dir, definition_items, IELIB_STRUCTURES_DIR)
+    PREFIX_DIR = get_format_version(ff)
+    dump_definition(PREFIX_DIR, definition_items, IELIB_STRUCTURES_DIR)
 
 # feature block
 fpath = os.path.join(IESDP_FILE_FORMATS_DIR, "itm_v1", "feature_block.yml")
